29f42280a359261772d708e50306b4326067a5c0
MatiiChoque <choquematia@gmail.com>
2023-06-21 18:28:13 -0300

    navegacion a los diferentes details

--- nextjsfirstapp/app/page.jsx
+++ nextjsfirstapp/app/page.jsx
@@ -1,3 +1,5 @@
+import Users from "@/components/Users";
+
 async function fetchUsers() {
   const res = await fetch("https://reqres.in/api/users");
   const data = await res.json();
@@ -9,17 +11,7 @@ export default async function IndexPage() {
   return (
     <div>
       <h1>Index Page</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <div>
-              <h5>{user.id}</h5>
-              <p>{user.email}</p>
-            </div>
-            <img src={user.avatar} alt={user.email} />
-          </li>
-        ))}
-      </ul>
+      <Users users={users} />
     </div>
   );
 }
