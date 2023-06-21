331bc41ba06cf205a140e8205e708e02a9e430dc
MatiiChoque <choquematia@gmail.com>
2023-06-21 18:44:01 -0300

    detail update

--- nextjsfirstapp/app/users/[id]/page.jsx
+++ nextjsfirstapp/app/users/[id]/page.jsx
@@ -1,3 +1,16 @@
-export default function UserPage() {
-  return <div>User Details</div>;
+async function getUser(id) {
+  console.log(id);
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+  return data.data;
+}
+
+export default async function UserPage({ params }) {
+  const user = await getUser(params.id);
+  return (
+    <div>
+      <h1>User Details</h1>
+      {JSON.stringify(user)}
+    </div>
+  );
 }
