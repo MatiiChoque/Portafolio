d43e825f583fe7e53d776a7d47f7872ee906dfd6
MatiiChoque <choquematia@gmail.com>
2023-06-23 16:33:28 -0300

    agregando resumen e imagen

--- nextjsfirstapp/app/page.jsx
+++ nextjsfirstapp/app/page.jsx
@@ -9,8 +9,29 @@ async function fetchUsers() {
 export default async function IndexPage() {
   const users = await fetchUsers();
   return (
-    <div>
-      <h1>Index Page</h1>
-    </div>
+    <header className="row">
+      <div className="card card-body bg-secondary text-light">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="MATIASCHOQUE.jpg"
+              alt=""
+              className="img-fluid"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+          <div className="col-md-8">
+            <h1>Matias Choque</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Consequatur, quis distinctio similique neque ipsam iusto! Nostrum,
+              praesentium dicta voluptatibus sequi consectetur delectus est
+              deleniti earum sunt similique nihil provident culpa?
+            </p>
+            <a href="/hireme">Contratame!</a>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
