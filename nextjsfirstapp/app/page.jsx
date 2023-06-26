f2c2c89852eb47b10ce581bb5e4b88e986f1535d
MatiiChoque <choquematia@gmail.com>
2023-06-26 19:01:49 -0300

    agregando skills

--- nextjsfirstapp/app/page.jsx
+++ nextjsfirstapp/app/page.jsx
@@ -1,4 +1,5 @@
 import Users from "@/components/Users";
+import { skills } from "@/profile";
 
 async function fetchUsers() {
   const res = await fetch("https://reqres.in/api/users");
@@ -9,30 +10,63 @@ async function fetchUsers() {
 export default async function IndexPage() {
   const users = await fetchUsers();
   return (
-    <header className="row">
-      <div className="card card-body bg-secondary text-light">
-        <div className="row">
-          <div className="col-md-4">
-            <img
-              src="MATIASCHOQUE.jpg"
-              alt=""
-              className="img-fluid"
-              style={{ width: "100%", height: "100%" }}
-            />
+    <>
+      <header className="row">
+        <div className="card card-body bg-secondary text-light">
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src="MATIASCHOQUE.jpg"
+                alt=""
+                className="img-fluid"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+            <div className="col-md-8">
+              <h1>Matias Choque</h1>
+              <h3>Full Stack Developer</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Consequatur, quis distinctio similique neque ipsam iusto!
+                Nostrum, praesentium dicta voluptatibus sequi consectetur
+                delectus est deleniti earum sunt similique nihil provident
+                culpa?
+              </p>
+              <a href="/hireme">Contratame!</a>
+            </div>
           </div>
-          <div className="col-md-8">
-            <h1>Matias Choque</h1>
-            <h3>Full Stack Developer</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consequatur, quis distinctio similique neque ipsam iusto! Nostrum,
-              praesentium dicta voluptatibus sequi consectetur delectus est
-              deleniti earum sunt similique nihil provident culpa?
-            </p>
-            <a href="/hireme">Contratame!</a>
+        </div>
+      </header>
+      {/*Segunda Seccion*/}
+      <div className="row py-2">
+        <div className="col-md-4">
+          <div className="card bd-ligth">
+            <div className="card-body">
+              <h1>Tecnologias</h1>
+
+              {skills?.map(({ skill, percentage }, i) => (
+                <div className="py-3" key={i}>
+                  <h5>{skill}</h5>
+                  <div className="progress">
+                    <div
+                      className="progress-bar"
+                      role="progressbar"
+                      style={{ width: `${percentage}%` }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="card bd-ligth">
+            <div className="card-body">
+              <h1>Experiencia</h1>
+            </div>
           </div>
         </div>
       </div>
-    </header>
+    </>
   );
 }
---
+++ nextjsfirstapp/profile.js
@@ -0,0 +1,22 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 80,
+  },
+  {
+    skill: "React.js",
+    percentage: 70,
+  },
+  {
+    skill: "Mongodb",
+    percentage: 50,
+  },
+  {
+    skill: "Node.js",
+    percentage: 80,
+  },
+  {
+    skill: "Next.js",
+    percentage: 20,
+  },
+];
