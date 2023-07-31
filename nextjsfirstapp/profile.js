9e488be254f0f22116011ee294b3ac3508160c59
MatiiChoque <choquematia@gmail.com>
2023-07-31 16:58:39 -0300

    mejorando el detail

--- nextjsfirstapp/profile.js
+++ nextjsfirstapp/profile.js
@@ -53,7 +53,7 @@ export const experience = [
   {
     title: "Encargado administrativo - Finanzas - Hospital Militar",
     description:
-      "Rendicion, gestión y control de combustibles, lubricantes y viveres. Me desempeñé como encargado, logrando un aumento significativo en el rendimiento y organización del área, como así también dejando precedentes de un adaptable y marcado modo de gestión sobre una innovadora aplicación web implementada a comienzos del año 2014.",
+      "Rendición, gestión y control de combustibles, lubricantes y víveres. Me desempeñé como encargado, logrando un aumento significativo en el rendimiento y organización del área, como así también dejando precedentes de un adaptable y marcado modo de gestión sobre una innovadora aplicación web implementada a comienzos del año 2014.",
     from: "Sep 2013",
     to: "Jul 2022",
   },
@@ -67,7 +67,7 @@ export const experience = [
   {
     title: "Countries App",
     description:
-      "Esta aplicación web está basada en React y está creada para tener un lugar de consulta sobre paises de todo el mundo permitiendo poder buscar, filtrar y ver detalles de cada país junto con sus actividades correspondientes. Utiliza la API de Countries para mostrar información existente y permite la creación de nuevas actividades y almacenarlos en una base de datos PostgreSQL con los detalles personalizados. Para poder encontrar el País o sus actividades podemos ordenarlo alfabéticamente, por población o el tipo de actividad, también dispone de la posibilidad de ver detalles de cada País, como cantidad de población, capital, continente y actividades, etc.",
+      "Esta aplicación web está basada en React y está creada para tener un lugar de consulta sobre países de todo el mundo permitiendo poder buscar, filtrar y ver detalles de cada país junto con sus actividades correspondientes. Utiliza la API de Countries para mostrar información existente y permite la creación de nuevas actividades y almacenarlos en una base de datos PostgreSQL con los detalles personalizados. Para poder encontrar el País o sus actividades podemos ordenarlo alfabéticamente, por población o el tipo de actividad, también dispone de la posibilidad de ver detalles de cada País, como cantidad de población, capital, continente y actividades, etc.",
     from: "Abr 2023",
     to: "May 2023",
   },
@@ -113,34 +113,25 @@ export const projects = [
   {
     id: 2,
     name: "AppCountries",
-    description: `Esta aplicación web está creada para tener un lugar de consulta sobre paises de todo el mundo permitiendo poder buscar, filtrar y ver detalles de cada país.`,
+    description: `Esta aplicación web está creada para tener un lugar de consulta sobre países de todo el mundo.`,
     detail: {
       funcionalidades: [
-        "Autenticación local y de terceros.",
+        "REST API",
         "Búsquedas.",
         "Filtros combinados.",
         "Ordenamientos.",
-        "Sistema de administración de usuarios.",
-        "Gestión y alojamiento de imágenes.",
-        "Pasarela de pagos.",
-        "Envío de emails.",
+        "Base de datos PostgreSQL",
       ],
-      cuerpo: `Formé parte del Back junto a otra persona, en donde pude realizar mis contribuciones organizando los servidores , agregando varias funcionalidades al sistema y ayudando en la gestión del funcionamiento de la pagina web, creamos la base de datos con MongoDB. 
+      cuerpo: `Proyecto individual en Henry - Utiliza la API de Countries para mostrar información existente y permite la creación de nuevas actividades y almacenarlos en una base de datos PostgreSQL con los detalles personalizados. Para poder encontrar el País o sus actividades podemos ordenarlo alfabéticamente, por población o el tipo de actividad, también dispone de la posibilidad de ver detalles de cada País, como cantidad de población, capital, continente y actividades, etc. 
       `,
       tecnologias: {
-        front: ["React.js", "Redux", "Axios", "Typescript", "Sass"],
+        front: ["React.js", "Redux", "Axios", "Javascript", "CSS", "HTML"],
         back: [
           "Javascript",
           "Node.js",
-          "Mongoose",
-          "Multer",
+          "PostgreSQL",
           "Express.js",
-          "Bcrypt",
-          "JWT",
-          "Mercado Pago",
-          "Google",
-          "Nodemailer",
-          "Cloudinary",
+          "Sequelize",
         ],
       },
     },
@@ -151,17 +142,10 @@ export const projects = [
     name: "Portafolio",
     description: `Considero a mi portafolio como un proyecto en sí mismo, ya que tuve la oportunidad de aplicar la mayor parte de los nuevos conocimientos que adquirí como desarrollador web.`,
     detail: {
-      funcionalidades: [
-        "Autenticación local y de terceros.",
-        "Búsquedas.",
-        "Filtros combinados.",
-        "Ordenamientos.",
-        "Sistema de administración de usuarios.",
-        "Gestión y alojamiento de imágenes.",
-        "Pasarela de pagos.",
-        "Envío de emails.",
-      ],
-      cuerpo: `Formé parte del Back junto a otra persona, en donde pude realizar mis contribuciones organizando los servidores , agregando varias funcionalidades al sistema y ayudando en la gestión del funcionamiento de la pagina web, creamos la base de datos con MongoDB. 
+      funcionalidades: [],
+      cuerpo: `Me complace presentar mi portfolio recientemente diseñado. Es el resultado de la mejora continua, la exploración de nuevos enfoques de diseño y la búsqueda de soluciones óptimas. El diseño prioriza las interacciones fáciles de usar para garantizar una experiencia de navegación optima. Me esfuerzo por brindar la mejor experiencia de usuario posible, y este portfolio es un testimonio de mi compromiso continuo con la excelencia en el desarrollo web.
+
+      El principal desafío era mantener la aplicación adaptable y atractiva para todos los tamaños de pantalla, desde un móvil pequeño hasta pantallas grandes. En este proyecto aprendí a trabajar de manera más efectiva y rápida. Como resultado, logre crear un producto que se adapta exitosamente a cualquier dispositivo, manteniendo el balance y la integridad sin dejar de lado la funcionalidad y la estética. 
       `,
       tecnologias: {
         front: ["React.js", "Redux", "Axios", "Typescript", "Sass"],
